c10f5eaf3254a4c37a5faeadf26555ee0c62b451
Theo <theodelmas.pro@gmail.com>
2021-05-10 16:23:33 +0200

    Second version

---
+++ src/Entity/Game.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Entity;
+
+class Game
+{
+    public $id;
+    public $name;
+    public $desc;
+    public $releaseYear;
+    public $platform;
+    public $img;
+    public $price;
+}
